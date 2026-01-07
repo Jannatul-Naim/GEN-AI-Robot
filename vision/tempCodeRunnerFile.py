c976b40ea0bc602303be5ec8ac4a7d4c8e935d71
Jannatul-Naim-Apu <japu2310475@bscse.uiu.ac.bd>
2026-01-07 11:55:57 +0600

    optimized

--- vision/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-                    obj = {
-                        "name": name,
-                        "confidence": round(conf, 2),
-                        "distance_cm": round(z_cm, 1),
-                        "center": [cx, cy],
-                        "grasp_center": [cx + GRASP_OFFSET_X, cy + GRASP_OFFSET_Y]
-                    }
-                    objects.append(obj)
